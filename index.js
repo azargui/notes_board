170a0fda59edad1852fa21a613713ba705f0acf3
RedaMhd <mouhdi.reda1@gmail.com>
2026-02-13 12:17:28 +0100

    add registeration route

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import authRouter from "./routes/auth.js";
 
 //db connection
 import "./config/db.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ data: "home page" });
-});
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use(authRouter);
 
 //running server
 const PORT = process.env.PORT || 3005;
