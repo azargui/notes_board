3bfe1bd4311b5cf792b651858ef7fc806b929601
mbarek-hani <mbarekhani30@gmail.com>
2026-02-13 11:28:25 +0100

    fix typos and format document

--- models/Note.js
+++ models/Note.js
@@ -1,25 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const noteSchema = new mongoos.Schema({
+const noteSchema = new mongoose.Schema(
+  {
     body: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     colors: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     position: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User", // name of the User model
-        // required: true,
-        index: true
-    }
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // name of the User model
+      // required: true,
+      index: true,
+    },
+  },
+  { timestamps: true },
+);
 
-}, { timestamps: true });
-
-export default mongoose.model('Note', noteSchema);
+export default mongoose.model("Note", noteSchema);
